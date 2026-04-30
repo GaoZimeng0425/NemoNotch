cafa4c2e6d950bc7f18cf659ed85e261fddb6700
GaoZimeng <gaozimeng0425@gmail.com>
2026-04-30 17:27:47 +0800

    feat(settings): change default tab to .overview

--- NemoNotch/Models/AppSettings.swift
+++ NemoNotch/Models/AppSettings.swift
@@ -62,7 +62,7 @@ final class AppSettings {
 
     init() {
         let storedTab = UserDefaults.standard.string(forKey: "defaultTab").flatMap { Tab(rawValue: $0) }
-        self.defaultTab = storedTab ?? .media
+        self.defaultTab = storedTab ?? .overview
 
         let storedTabs = UserDefaults.standard.stringArray(forKey: "enabledTabs")?.compactMap { Tab(rawValue: $0) }
         let tabs = storedTabs.map(Set.init) ?? Set(Tab.allCases)
